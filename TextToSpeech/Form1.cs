e0c3ed298351f8679dee15a2f91a8cea57755fe5
Spencer Plant <splant@ualberta.ca>
2015-12-13 01:32:15 -0700

    first commit - added basic form

---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by Spencer Plant Dec 13 2015
+ * A text to speech app that allows a user to paste in text to save as a .wav file for later listening
+ * Heavy use of SpeechSynthesizer, and this reference:
+ * https://msdn.microsoft.com/en-us/library/ms586885(v=vs.110).aspx
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
